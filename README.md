5fa8e3e17d0cf0a2f08b34eb39eeccd4c08e8939
Shavez Khan <141126969+shavez0022@users.noreply.github.com>
2024-01-16 14:46:47 +0530

    Add files via upload
    It is a basic portfolio made using html, css, type.js which is very reactive and intresting.

---
+++ README.md
@@ -0,0 +1,2 @@
+
+It is a basic portfolio made using html, css, type.js which is very reactive and intresting.
---
+++ main.js
@@ -0,0 +1,4 @@
+var typingEffect = new Typed(".mt",{
+
+    strings:["coder","developer","learner","hustler"],loop:true,typeSpeed:100,backSpeed:80,backDelay:1500
+})
